9b4c19e704b6301b840fef11a1aa81e0fe86eacd
dodunge <dodunge@gmail.com>
2023-03-29 00:41:14 +0900

    2.0 environment settings

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBUsfec5lHnE7MO8tBw3xuIPwyai28PLpA",
+    authDomain: "nwitter-c341f.firebaseapp.com",
+    projectId: "nwitter-c341f",
+    storageBucket: "nwitter-c341f.appspot.com",
+    messagingSenderId: "112503887510",
+    appId: "1:112503887510:web:28ea88e2aebb6a9e2494b7"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
