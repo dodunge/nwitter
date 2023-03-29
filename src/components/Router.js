2265fd221e89e7d0734d3222a32b6e65aea348b6
dodunge <dodunge@gmail.com>
2023-03-29 19:40:55 +0900

    4.0 LogIn Join SocialLogIn

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,24 @@
-import { useState } from "react";
+//import { HashRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
-const AppRouter = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({ isLoggedIn }) => {
     return(
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
-                    <Route path="/" element={ <Home /> } />
+                    <>
+                        <Route path="/" element={ <Home /> } />
+                        <Route path="/profile" element={ <Profile /> } />
+                    </>
                 ) : (
                     <Route path="/" element={ <Auth /> } />                 
                 )}
+                {/* <Route path="*" element={ <Navigate replace to="/" /> } /> */}
             </Routes>
         </Router>
     );
